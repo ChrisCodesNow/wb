dd94bb4fc6f41725600e1644e7b60b9bd17fafad
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-05-15 18:17:27 -0500

    Complete binary search

---
+++ 03_greedy_recursion_dynamic/02_binary_search/01_binary_search.py
@@ -0,0 +1,47 @@
+'''
+Approach 1:
+    Check mid num, either too high or too low
+Runtime: O(logn)
+Space Complexity: O(1)
+'''
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            if target > nums[mid]:
+                start += 1
+            elif target < nums[mid]:
+                end -= 1
+            else:
+                return mid
+
+        return -1
+
+
+# Test
+class Test:
+    count = 0
+    def run(self, result):
+        self.count += 1
+        if result:
+            print(f"Passed test {self.count}")
+        else:
+            print(f"Failed test {self.count}")
+
+        
+if __name__ == '__main__':
+    s = Solution()
+    t = Test()
+
+
+    nums = [-1,0,3,5,9,12]
+    target = 9
+    t.run(s.search(nums, target) == 4)
+
+    nums = [-1,0,3,5,9,12]
+    target = 2
+    t.run(s.search(nums, target) == -1)
