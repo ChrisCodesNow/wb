5abf418800fb88f67e5db0c4c17be5d3131f38d1
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-05-19 18:14:24 -0500

    Add second approach

--- 03_greedy_recursion_dynamic/04_recursion_memo/03_word_distance.py
+++ 03_greedy_recursion_dynamic/04_recursion_memo/03_word_distance.py
@@ -13,15 +13,26 @@ Approach 1:
 
 Runtime: O(TBD)
 Space Complexity: O(TBD)
+
+Approach 2:
+    Same as approach 1, but memoize solution of current
+    subproblem to avoid repeated computations
+
+Runtime: O(TBD)
+Space Complexity: O(TBD)
 '''
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        return self.edit_distance_01(word1, word2, 0, 0)
-
+        # return self.solution_01(word1, word2)
+        return self.solution_02(word1, word2)
 
     # ########################################
     # Approach 1
     #
+    def solution_01(self, word_1, word_2):
+        return self.edit_distance_01(word_1, word_2, 0, 0)
+
+
     def edit_distance_01(self, word_1, word_2, i, j):
         if i == len(word_1) and j == len(word_2):
             return 0
@@ -36,6 +47,36 @@ class Solution:
             return 1 + min(self.edit_distance_01(word_1, word_2, i +1, j + 1),
                         self.edit_distance_01(word_1, word_2, i, j + 1),
                         self.edit_distance_01(word_1, word_2, i + 1, j))
+
+
+    # ########################################
+    # Approach 2
+    #
+    def solution_02(self, word_1, word_2):
+        memo = dict()
+        return self.edit_distance_02(word_1, word_2, 0, 0, memo)
+
+
+    def edit_distance_02(self, word_1, word_2, i, j, memo):
+        if (i, j) in memo:
+            return memo[(i, j)]
+
+        if i == len(word_1) and j == len(word_2):
+            return 0
+        elif i == len(word_1):  # insert remaining chars of word_2
+            memo[(i, j)] = 1 + self.edit_distance_02(word_1, word_2, i, j + 1, memo)
+        elif j == len(word_2):  # delete remaining chars of word_1
+            memo[(i, j)] = 1 + self.edit_distance_02(word_1, word_2, i + 1, j, memo)
+        
+        elif word_1[i] == word_2[j]:
+            memo[(i, j)] = self.edit_distance_02(word_1, word_2, i + 1, j + 1, memo)
+        else:
+            memo[(i, j)] = 1 + \
+            min(self.edit_distance_02(word_1, word_2, i + 1, j + 1, memo),
+                self.edit_distance_02(word_1, word_2, i, j + 1, memo), 
+                self.edit_distance_02(word_1, word_2, i + 1, j, memo) )
+
+        return memo[(i, j)]
 
 
 # Test
