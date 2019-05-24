681e4932c9d10a8e0566c3f5de053f4308b4f1ba
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-05-24 15:31:34 -0500

    Add second approach, dynamic programming

--- 03_greedy_recursion_dynamic/05_dynamic_programming/01_power.py
+++ 03_greedy_recursion_dynamic/05_dynamic_programming/01_power.py
@@ -1,13 +1,20 @@
 '''
-Approach 1:
+Approach 1: Bottom up
     Fill array of sub powers, eventually reaching power x, n
 Runtime: O(n)
 Space Complexity: O(n)
+
+Approach 2: Dynamic Programming
+    Solve subproblems bottom, only storing solution to subproblems 
+    that will help compute next even and next odd powers.
+
+Runtime: O(n)
+Space Complexity: O(1)
 '''
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return self.solution_01(x, n)
-
+        # return self.solution_01(x, n)
+        return self.solution_02(x, n)
 
     # ########################################
     # Approach 1
@@ -29,6 +36,36 @@ class Solution:
             return powers[n]
 
 
+    # ########################################
+    # Approach 2
+    #
+    def solution_02(self, x, n):
+        is_negative = n < 0
+        
+        n = abs(n)
+        if n == 0:
+            return 1
+        elif n == 1:
+            if is_negative:
+                return 1 / x
+            else:
+                return x
+
+        next_odd = 1           # Help compute next even and odd
+        next_even = x
+        for i in range(2, n + 1):
+            if i % 2 == 0:
+                power = next_even * next_even
+                next_odd *= x * x
+                next_even *= x
+            else:
+                power = next_odd * x
+
+        if is_negative:
+            return 1 / power
+        else:
+            return power
+
 # Test
 class Test:
     count = 0
@@ -39,11 +76,10 @@ class Test:
         else:
             print(f"Failed test {self.count}")
 
-        
+    
 if __name__ == '__main__':
     s = Solution()
     t = Test()  
-
 
     x = 2.00000
     n = 10
