33552c548c526988869dfc90a0a3f6844ce7a8d6
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-06-30 15:16:11 -0500

    Correctly complete islands

--- 01_basics/06_union_find/02_islands.py
+++ 01_basics/06_union_find/02_islands.py
@@ -1,139 +1,76 @@
 '''
 Approach 1:
-    Modified bfs, count distinct lands
-
-Runtime: O(mn)
-Space Complexity: O(mn)
-
-Note: Verify runtinme and space complexity
+    Traverse grid:
+        Connect land to adjacent land using disjoint set.
+        Count distinct root elements that happen to be land.
+Runtime: O(n^2)
+Space Complexity: O(n^2)
 '''
-from typing import List
 from union_find import UnionFind
-from collections import deque
-
+from typing import List
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        ds = self.modified_bfs(grid)
-        return self.count_distinct_islands(grid, ds)
 
+        ds = self.connect_lands(grid)
+        return self.count_disjoint_islands(grid, ds)
     
-    # Build disjoint set of connected components
-    def modified_bfs(self, grid):
-        visited = set()
-        Q = deque(self.get_land_coordinates(grid))
-        
-        # BFS modification
-        m, n = self.grid_dimmensions(grid)
-        ds = UnionFind(n * m)
 
-        # first grid element
-        visited.add((0, 0))
-        Q.append((0, 0))
+    def connect_lands(self, grid):
+        m,n = len(grid), len(grid[0])
+        ds = UnionFind(m * n)
 
-        while Q:
-            u = Q.popleft()
-            for v in self.get_neighbors(u, grid):
-                if v not in visited:
-                    if self.are_adjacent_land(u, v, grid):
-                        self.connect_land(u, v, ds, grid)
+        for i,row in enumerate(grid):
+            for j,val in enumerate(row):
+                coord = (i,j)
+                if self.is_land(grid, coord):
+                    for v in self.get_neighbors(grid, coord):
+                        if self.is_land(grid, v):
+                            first = self.num(grid, coord)
+                            second = self.num(grid, v)
+                            ds.join(first, second)
 
-                    visited.add(v)
-
+        ds.compress()
         return ds
 
-    
-    # Count islands, ignoring bodies of water
-    def count_distinct_islands(self, grid, ds):
-        ds.compress()
-        distinct_subsets = set(ds.get_parent())
 
-        m, n = self.grid_dimmensions(grid)
-        count = 0
-        for ele_id in distinct_subsets:
-            i = self.id_row(ele_id, n)
-            j = self.id_col(ele_id, n)
-            if grid[i][j]:
-                count += 1
-
-        print(ds.get_parent())
-        return count
+    def is_land(self, grid, coord):
+        i,j = coord
+        return grid[i][j] == '1'
 
 
-    def grid_dimmensions(self, grid):
-        rows = len(grid)
-        cols = len(grid[0])
-        return rows, cols
+    def get_neighbors(self, grid, coord):
+        m,n = len(grid), len(grid[0])
+        i,j = coord
 
-
-    # Get valid neighbors of input grid element
-    def get_neighbors(self, u, grid):
-        i, j = u
-        m, n = self.grid_dimmensions(grid)
-
-        # Up, down, left, right
-        if 0 <= i - 1:
-            yield (i - 1, j)
         if i + 1 < m:
             yield (i + 1, j)
-        if 0 <= j - 1:
-            yield (i, j - 1)
         if j + 1 < n:
             yield (i, j + 1)
 
 
-    # Get all land coordinates on grid
-    def get_land_coordinates(self, grid):
-        land_coordinates = []
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]:
-                    land_coordinates.append((i, j))
+    def num(self, grid, coord):
+        n = len(grid[0])
+        i,j = coord
 
-        return land_coordinates
+        return i * n + j
 
 
-    # Check if adjacent elements are land
-    def are_adjacent_land(self, u, v, grid):
-        i_u, j_u = u
-        i_v, j_v = v
+    def count_disjoint_islands(self, grid, ds):
+        roots = set(ds.get_parent())
+        count = 0
+        for num in roots:
+            coord = self.to_coordinates(grid, num)
+            if self.is_land(grid, coord):
+                count += 1
 
-        if grid[i_u][j_u] and grid[i_v][j_v]:
-            return True
-        
-        return False
-
-
-    # Join adjacent subsets of land
-    def connect_land(self, u, v, ds, grid):
-        i_u, j_u = u
-        i_v, j_v = v
-        m, n = self.grid_dimmensions(grid)
-
-        id_u = self.row_col_id(u, n)
-        id_v = self.row_col_id(v, n)
-
-        ds.join(id_u, id_v)
+        return count
 
 
-    # Get enumerated grid element's row value
-    def id_row(self, ele_id, cols):
-        return ele_id // cols
-
-
-    # Get enumerated grid element's col value
-    def id_col(self, ele_id, cols):
-        return ele_id % cols
-
-
-    # Get enumerated grid element from its row and columns
-    def row_col_id(self, u, cols):
-        i, j = u
-        ele_id = cols * i + j
-
-        return ele_id
-
+    def to_coordinates(self, grid, num):
+        n = len(grid[0])
+        return num // n, num % n
 
 
 # Test
@@ -151,24 +88,15 @@ if __name__ == '__main__':
     s = Solution()
     t = Test()
 
-
-# BUG: id(8) is not being tied to main island..
-# [3, 3, 3, 3, 4, 3, 3, 7, 8, 9, 3,   3, 12, 13, 14, 15, 16, 17, 18, 19]
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-    grid = [[1,1,1,1,0],
-            [1,1,0,1,0],
-            [1,1,0,0,0],
-            [0,0,0,0,0]]
-    t.run(s.numIslands(grid) == 1)
-    
-
-    grid = [[1,1,0,0,0],
-            [1,1,0,0,0],
-            [0,0,1,0,0],
-            [0,0,0,1,1]]
-
-    t.run(s.numIslands(grid) == 3)
-
+    grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+    ]
+    count = 1
+    t.run(s.numIslands(grid) == count)
 
     grid = []
-    t.run(s.numIslands(grid) == 0)
+    count = 0
+    t.run(s.numIslands(grid) == count)
