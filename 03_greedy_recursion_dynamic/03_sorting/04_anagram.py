dda337f5131e176ec91e6679ae90b20c3037ddf6
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-07-03 20:40:46 -0500

    Complete find all anagrams
    Use comparison based sorting

--- 03_greedy_recursion_dynamic/03_sorting/04_anagram.py
+++ 03_greedy_recursion_dynamic/03_sorting/04_anagram.py
@@ -1,37 +1,47 @@
 '''
 Approach 1:
-    Build every substring of size |p|, checking for anagrams
-    Edge Case: |p| > |s| => stop
-Runtime: O(|s| |p|log|p|)
-Space Complexity: O(|p|)
+    S empty => No anagrams
+
+    front = 0
+    back = len(p) - 2
+    Sort p.
+    window = Dummy_front + S from [front up to back].
+
+    Traverse S as windows:
+        Move front and back to next values.
+        Remove window's left value.
+        Add current back character to window.
+        Sorted window = sorted p:
+            Save start index.
+    
+    Get all saved indices.
+
+Runtime: O(S * PlogP)
+Space Complexity: O(S)
 '''
 from typing import List
+from collections import deque
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p) > len(s):
+        if not s or len(s) < len(p):
             return []
 
-        front = 0
-        back = len(p)
         indices = []
-        while back <= len(s):
-            if s == 'abab':
-                print(front, back)
-            window = s[front:back]
-            if self.are_anagrams(window, p):
+        sorted_p = sorted(p)
+        initial_s = list(s[:len(p) - 1])
+        window = deque(["dummy"] + initial_s)
+
+        back = len(p) - 2
+        last_front = len(s) - len(p)
+        for front in range(last_front + 1):
+            back += 1
+            window.popleft()
+            window.append(s[back])
+            if sorted(window) == sorted_p:
                 indices.append(front)
 
-            front += 1
-            back += 1
-
         return indices
-
-
-    def are_anagrams(self, s, p):
-        return sorted(s) == sorted(p)
-
-
-
+        
 # Test
 class Test:
     count = 0
@@ -44,20 +54,19 @@ class Test:
 
         
 if __name__ == '__main__':
-    s = Solution()
-    t = Test()
+    solution = Solution()
+    test = Test()
 
 
-    string_s = "cbaebabacd" 
-    string_p = "abc"
-    t.run(s.findAnagrams(string_s, string_p) == [0, 6])
-
-    string_s = "abab" 
-    string_p = "ab"
-    t.run(s.findAnagrams(string_s, string_p) == [0, 1, 2])
+    s = "cbaebabacd" 
+    p = "abc"
+    result = [0, 6]
+    my_result = solution.findAnagrams(s, p)
+    test.run(result == my_result)
 
 
-
-
-
-    
+    s = "abab" 
+    p = "ab"
+    result = [0, 1, 2]
+    my_result = solution.findAnagrams(s, p)
+    test.run(result == my_result)
