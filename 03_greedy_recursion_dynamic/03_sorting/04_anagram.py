3d70aacacc04a0337b24203f250401a8d634819f
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-07-07 17:46:46 -0500

    Find all anagrams
    Add second approach, use histogram component of count sort, better runtime than comparison based sorting

--- 03_greedy_recursion_dynamic/03_sorting/04_anagram.py
+++ 03_greedy_recursion_dynamic/03_sorting/04_anagram.py
@@ -18,11 +18,34 @@ Approach 1:
 
 Runtime: O(S * PlogP)
 Space Complexity: O(S)
+
+
+Approach 2:
+    Similar as approach 1, but use count sort instead of comparison based sorting
+
+    Use a window of size P
+    Traverse S using window:
+        Add next right value from S
+        Sorted window is same as sorted P:
+            Save current left index
+        Remove left value of window
+
+    Get all saved incides
+Runtime: O(SP)
+Space Complexity: O(S + P)
 '''
 from typing import List
 from collections import deque
+from count_sort import get_histogram
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # return self.solution_01(s, p)
+        return self.solution_02(s, p)
+
+    # ########################################
+    # Approach 1
+    #
+    def solution_01(self, s, p):
         if not s or len(s) < len(p):
             return []
 
@@ -41,7 +64,54 @@ class Solution:
                 indices.append(front)
 
         return indices
+
         
+    # ########################################
+    # Approach 2
+    #
+    def solution_02(self, s, p):
+        if not s or len(s) < len(p):
+            return []
+
+        alphabet_size = 26
+
+        window = self.to_indices(s[:len(p) - 1])
+        sorted_window = get_histogram(window, alphabet_size)
+        sorted_p = get_histogram(self.to_indices(p), alphabet_size)
+
+        right = len(p) - 2
+        num_windows = len(s) - len(p) + 1
+        anagram_indices = []
+        for left in range(num_windows):
+            right += 1
+            self.increment(sorted_window, s[right])
+            if sorted_window == sorted_p:
+                anagram_indices.append(left)
+
+            self.decrement(sorted_window, s[left])
+
+        return anagram_indices
+
+
+
+    def to_indices(self, alphabet_chars):
+        return list(map(self.idx, alphabet_chars))
+
+
+    def increment(self, histogram, char):
+        i = self.idx(char)
+        histogram[i] += 1
+
+    
+    def decrement(self, histogram, char):
+        i = self.idx(char)
+        histogram[i] -= 1
+
+
+    def idx(self, char):
+        return ord(char) - ord('a')
+
+
 # Test
 class Test:
     count = 0
