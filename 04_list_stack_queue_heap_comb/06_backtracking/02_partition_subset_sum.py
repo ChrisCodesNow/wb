ee2cf5def2dba84b1e1f4d586fdea1ee031e0f7e
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-07-20 18:40:25 -0500

    Attempt 02 to partition subset sum

--- 04_list_stack_queue_heap_comb/06_backtracking/02_partition_subset_sum.py
+++ 04_list_stack_queue_heap_comb/06_backtracking/02_partition_subset_sum.py
@@ -6,6 +6,13 @@ Space Complexity: O()
 from typing import List
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        # return self.solution_01(nums)
+        return self.solution_02(nums)
+        
+    # ########################################
+    # Approach 1
+    #
+    def solution_01(self, nums):
         nums_sum = sum(nums)
         if nums_sum % 2 != 0:
             return False
@@ -27,8 +34,36 @@ class Solution:
 
 
     # ########################################
-    # Approach 1
+    # Approach 2
     #
+    def solution_02(self, nums):
+        nums_sum = sum(nums)
+        if nums_sum % 2 != 0:
+            return False
+            
+        target = sum(nums) / 2
+        memo = dict()
+        return self.eqsum(nums, target, 0, 0, memo)
+
+
+    def eqsum(self, nums, target, total, start, memo):
+        if (start, total) in memo:
+            return memo[start, total]
+        elif total == target:
+            return True
+        elif start > len(nums) - 1:
+            return False
+        
+        for i in range(start, len(nums)):
+            if total + nums[i] <= target:
+                if self.eqsum(nums, target, total + nums[i], i + 1, memo):
+                    memo[start, total] = True
+                    return memo[start, total]
+        
+        memo[start, total] = False
+        return memo[start, total]
+        
+
 
 # Test
 class Test:
@@ -55,6 +90,17 @@ if __name__ == '__main__':
     my_result = solution.canPartition(nums)
     test.run(result == my_result)
 
+    nums = [99,2,3,98]
+    result = True
+    my_result = solution.canPartition(nums)
+    test.run(result == my_result)
+
+    nums = [17,58,41,75,61,70,52,7,38,11,40,58,44,45,4,81,67,54,79,80,15,3,14,16,9,66,69,41,72,37,28,3,33,90,56,12,72,49,35,22,49,27,49,82,41,77,100,82,18,95,24,51,37,2,34,82,70,53,73,32,90,98,81,22,73,76,79,40,27,62,45,96,36,15,63,28,54,88,63,37,58,9,62,98,93,72,99,53,91,29,61,31,11,42,20,35,50,68,10,86]
+    result = False
+    my_result = solution.canPartition(nums)
+    test.run(result == my_result)
+
+
     nums = [
         1,1,1,1,1,1,1,1,1,1,
         1,1,1,1,1,1,1,1,1,1,
@@ -68,7 +114,7 @@ if __name__ == '__main__':
 
 
     nums = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,100]
-    result = True
+    result = False
     my_result = solution.canPartition(nums)
     test.run(result == my_result)
 
