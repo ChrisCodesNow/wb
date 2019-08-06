b672c272bb7d211d49551b3a8d64d268f3b856d0
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-08-05 21:06:33 -0500

    Add attempt 02 DFS, remove in degree to add to Q

--- 05_trees_graphs/02_graphs/05_course_schedule_2.py
+++ 05_trees_graphs/02_graphs/05_course_schedule_2.py
@@ -23,6 +23,32 @@ Approach 1: BFS to get layer wise traversal and order
                     Add v to Q
 
         Get ordered result
+
+
+Approach 2: Similar to approach 1, but use in_g to remove in degree and keep track of which vertex to visit next
+        BFS to get layer wise traversal and order
+        Does this imply topological sort?
+
+    algo(courses, edges):
+        g = get adjacency list from edges
+        in_g = get in degree count from edges and num courses
+    
+        src = all vertices with in degree of 0 at in_g
+
+        Get BFS result @ (src, g, in_g, numCourses)
+
+    BFS(src, g, in_g, numCourses):
+        Q = Queue with all src elements
+
+        Iterate Q:
+            src - next in Q
+            Add src to ordered result
+            Iterate neighbors v of src in g:
+                Reduce in_g of v by 1
+                in_g[v] = 0?:
+                    Add to Q
+
+        Get ordered result
     
         
 Runtime: O()
@@ -32,7 +58,8 @@ from typing import List
 from collections import defaultdict, deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        return self.solution_01(numCourses, prerequisites)
+        # return self.solution_01(numCourses, prerequisites)
+        return self.solution_02(numCourses, prerequisites)
 
 
     # ########################################
@@ -46,20 +73,6 @@ class Solution:
         return self.BFS(src, g, numCourses)
 
 
-    def adj_list(self, edges_rev):
-        g = defaultdict(set)
-        for v,u in edges_rev:
-            g[u].add(v)
-        return g
-
-
-    def in_deg(self, edges_rev):
-        in_g = defaultdict(int)
-        for v,u in edges_rev:
-            in_g[v] += 1
-        return in_g
-
-    
     def BFS(self, src, g, num_courses):
         order = []
         Q = deque(src[:])
@@ -78,7 +91,52 @@ class Solution:
                     Q.append(v)
 
         return order
-        
+
+
+    # ########################################
+    # Approach 2
+    #
+    def solution_02(self, numCourses, prerequisites):
+        g = self.adj_list(prerequisites)
+        in_g = self.in_deg(prerequisites)
+
+        src = [u for u in range(numCourses) if in_g[u] == 0]
+        return self.BFS_02(src, g, in_g, numCourses)
+
+
+    def BFS_02(self, src, g, in_g, num_courses):
+        order = []
+        Q = deque(src[:])
+
+        while Q:
+            u = Q.popleft()
+            order.append(u)
+            if len(order) == num_courses:
+                return order
+
+            for v in g[u]:
+                in_g[v] -= 1
+                if in_g[v] == 0:
+                    Q.append(v)
+
+        return order
+
+
+    # ########################################
+    # Shared methods
+    #
+    def adj_list(self, edges_rev):
+        g = defaultdict(set)
+        for v,u in edges_rev:
+            g[u].add(v)
+        return g
+
+
+    def in_deg(self, edges_rev):
+        in_g = defaultdict(int)
+        for v,u in edges_rev:
+            in_g[v] += 1
+        return in_g
 
 
 # Test
@@ -109,3 +167,10 @@ if __name__ == '__main__':
     result = [[0,1,2,3], [0,2,1,3]]
     my_result = solution.findOrder(num_courses, prerequisites)
     test.run(my_result in result)
+
+
+    num_courses = 3
+    prerequisites = [[0,1],[0,2],[1,2]]
+    result = [2,1,0]
+    my_result = solution.findOrder(num_courses, prerequisites)
+    test.run(my_result == result)
